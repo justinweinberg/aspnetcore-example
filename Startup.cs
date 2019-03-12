34f22ad556271e3b7c56066a2c135e44cb60471a
justinweinberg <justin@modrg.com>
2019-03-11 20:49:38 -0500

    added versioning for swagger

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,23 @@ namespace demo
         // Configure dependency injection and setup services
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApiVersioning(
+                 options =>
+                 {
+                     // reporting api versions will return the headers "api-supported-versions" and "api-deprecated-versions"
+                     options.ReportApiVersions = true;
+                 });
+            services.AddVersionedApiExplorer(
+                options =>
+                {
+                    // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                    // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
+                });
             //setup dependency injection for our Controllers
             services.AddTransient<IKartService, FakeKartService>();
             services.AddTransient<IHealthCheckService, FakeHealthCheckService>();
@@ -63,6 +80,7 @@ namespace demo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             //configure swagger middleware
             app.UseSwagger();
             //configure swagger UI middleware
@@ -74,6 +92,7 @@ namespace demo
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
             //configure MVC middleware
             app.UseMvc();
+
         }
     }
 }
